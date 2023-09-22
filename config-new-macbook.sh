89e44d4e6bac6696ee50819d214aa3da348c5f65
lowranceworks <lowranceworks@gmail.com>
2023-09-22 12:04:58 -0500

    fix: improve config-new-macbook.sh script

--- config-new-macbook.sh
+++ config-new-macbook.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-echo -e "\033[32minstalling homebrew...\033[0m"
-
 # install homebrew
+echo -e "\033[32minstalling homebrew...\033[0m"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-# add homebrew to path
 (echo; echo 'eval "$(/opt/homebrew/bin/brew)"') >> $HOME/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # install ansible
+echo -e "\033[32minstalling ansible...\033[0m"
 brew install ansible
 
-# make Projects directory
-mkdir ~/Projects
-
 # clone macbook_config project
-git clone https://github.com/lowranceworks/macbook_config.git
+echo -e "\033[32mcloning macbook_config project...\033[0m"
+mkdir ~/Projects
+git clone https://github.com/lowranceworks/macbook_config.git $HOME/Projects/macbook_config
 
-# set configuration
+# run set-config.yaml playbook
+echo -e "\033[32mrunning set-config.yaml playbook...\033[0m"
 ansible-playbook $HOME/Projects/macbook_config/playbooks/set-config.yaml
