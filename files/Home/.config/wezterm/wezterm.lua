a1d3a1e1ea2c1234ff0e387cc2a3331ce7500b6b
lowranceworks <lowranceworks@gmail.com>
2024-01-22 11:06:58 -0600

    chore: backup wezterm with advanced settings (including narutal text editing)

--- files/Home/.config/wezterm/wezterm.lua
+++ files/Home/.config/wezterm/wezterm.lua
@@ -1,29 +1,244 @@
+--
+-- ██╗    ██╗███████╗███████╗████████╗███████╗██████╗ ███╗   ███╗
+-- ██║    ██║██╔════╝╚══███╔╝╚══██╔══╝██╔════╝██╔══██╗████╗ ████║
+-- ██║ █╗ ██║█████╗    ███╔╝    ██║   █████╗  ██████╔╝██╔████╔██║
+-- ██║███╗██║██╔══╝   ███╔╝     ██║   ██╔══╝  ██╔══██╗██║╚██╔╝██║
+-- ╚███╔███╔╝███████╗███████╗   ██║   ███████╗██║  ██║██║ ╚═╝ ██║
+--  ╚══╝╚══╝ ╚══════╝╚══════╝   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝
+-- A GPU-accelerated cross-platform terminal emulator
+-- https://wezfurlong.org/wezterm/
+
+local b = require("utils/background")
+local cs = require("utils/color_scheme")
+local f = require("utils/font")
+local h = require("utils/helpers")
+local k = require("utils/keys")
+local w = require("utils/wallpaper")
+
 local wezterm = require("wezterm")
-local os = require("os")
+local act = wezterm.action
 
-local function get_home_dir()
-	return os.getenv("HOME")
-end
+local config = {
+	-- background = {
+	-- 	w.get_wallpaper(),
+	-- 	b.get_background(),
+	-- },
+	-- color_scheme = cs.get_color_scheme(),
+	color_scheme = "Dracula (Official)",
+	font = wezterm.font("MesloLGS NF"),
+	font_size = 16,
 
-return {
-	-- General settings
+	window_padding = {
+		left = 30,
+		right = 30,
+		top = 20,
+		bottom = 10,
+	},
+
+	set_environment_variables = {
+		-- THEME_FLAVOUR = "latte",
+		BAT_THEME = h.is_dark() and "Catppuccin-mocha" or "Catppuccin-latte",
+		TERM = "xterm-256color",
+	},
+
+	-- general options
 	adjust_window_size_when_changing_font_size = false,
 	debug_key_events = false,
 	enable_tab_bar = false,
 	native_macos_fullscreen_mode = false,
 	window_close_confirmation = "NeverPrompt",
-	window_decorations = "RESIZE", -- hides title bar buttons
+	window_decorations = "RESIZE",
 
-	-- Appearance settings
-	color_scheme = "Dracula (Official)",
-	font = wezterm.font("MesloLGS NF"),
-	font_size = 16,
+	-- keys
+	keys = {
+		-- enable natural text editing
+		{ mods = "OPT", key = "LeftArrow", action = wezterm.action.SendKey({ mods = "ALT", key = "b" }) },
+		{ mods = "OPT", key = "RightArrow", action = wezterm.action.SendKey({ mods = "ALT", key = "f" }) },
+		-- { mods = "CMD", key = "LeftArrow", action = wezterm.action.SendKey({ mods = "CTRL", key = "a" }) }, -- this is disabled because it shares the same hexcode as C-a (which is my tmux prefix)
+		{ mods = "CMD", key = "RightArrow", action = wezterm.action.SendKey({ mods = "CTRL", key = "e" }) },
+		{ mods = "CMD", key = "Backspace", action = wezterm.action.SendKey({ mods = "CTRL", key = "u" }) },
 
-	-- Background image
-	-- window_background_image = get_home_dir() .. "/.config/wezterm/wallpapers/space-moon.jpg",
+		k.cmd_key(".", k.multiple_actions(":ZenMode")),
+		k.cmd_key("[", act.SendKey({ mods = "CTRL", key = "o" })),
+		k.cmd_key("]", act.SendKey({ mods = "CTRL", key = "i" })),
+		k.cmd_key("f", k.multiple_actions(":Grep")),
+		k.cmd_key("H", act.SendKey({ mods = "CTRL", key = "h" })),
+		k.cmd_key("i", k.multiple_actions(":SmartGoTo")),
+		k.cmd_key("J", act.SendKey({ mods = "CTRL", key = "j" })),
+		k.cmd_key("K", act.SendKey({ mods = "CTRL", key = "k" })),
+		k.cmd_key("L", act.SendKey({ mods = "CTRL", key = "l" })),
+		k.cmd_key("P", k.multiple_actions(":GoToCommand")),
+		k.cmd_key("p", k.multiple_actions(":GoToFile")),
+		k.cmd_key("j", k.multiple_actions(":GoToFile")),
+		k.cmd_key("q", k.multiple_actions(":qa!")),
+		k.cmd_to_tmux_prefix("1", "1"),
+		k.cmd_to_tmux_prefix("2", "2"),
+		k.cmd_to_tmux_prefix("3", "3"),
+		k.cmd_to_tmux_prefix("4", "4"),
+		k.cmd_to_tmux_prefix("5", "5"),
+		k.cmd_to_tmux_prefix("6", "6"),
+		k.cmd_to_tmux_prefix("7", "7"),
+		k.cmd_to_tmux_prefix("8", "8"),
+		k.cmd_to_tmux_prefix("9", "9"),
+		k.cmd_to_tmux_prefix("`", "n"),
+		k.cmd_to_tmux_prefix("b", "B"),
+		k.cmd_to_tmux_prefix("C", "C"),
+		k.cmd_to_tmux_prefix("d", "D"),
+		k.cmd_to_tmux_prefix("G", "G"),
+		k.cmd_to_tmux_prefix("g", "g"),
+		k.cmd_to_tmux_prefix("k", "T"),
+		k.cmd_to_tmux_prefix("l", "L"),
+		k.cmd_to_tmux_prefix("n", '"'),
+		k.cmd_to_tmux_prefix("N", "%"),
+		k.cmd_to_tmux_prefix("o", "u"),
+		k.cmd_to_tmux_prefix("T", "!"),
+		k.cmd_to_tmux_prefix("t", "c"),
+		k.cmd_to_tmux_prefix("w", "x"),
+		k.cmd_to_tmux_prefix("z", "z"),
 
-	-- Background color with an opaque overlay (adjust the alpha value)
-	colors = {
-		background = "rgba(41, 42, 54, 1.0)", -- Semi-transparent dark overlay
+		k.cmd_key(
+			"R",
+			act.Multiple({
+				act.SendKey({ key = "\x1b" }), -- escape
+				k.multiple_actions(":source %"),
+			})
+		),
+
+		k.cmd_key(
+			"s",
+			act.Multiple({
+				act.SendKey({ key = "\x1b" }), -- escape
+				k.multiple_actions(":w"),
+			})
+		),
+
+		{
+			mods = "CMD|SHIFT",
+			key = "}",
+			action = act.Multiple({
+				act.SendKey({ mods = "CTRL", key = "b" }),
+				act.SendKey({ key = "n" }),
+			}),
+		},
+		{
+			mods = "CMD|SHIFT",
+			key = "{",
+			action = act.Multiple({
+				act.SendKey({ mods = "CTRL", key = "b" }),
+				act.SendKey({ key = "p" }),
+			}),
+		},
+
+		{
+			mods = "CTRL",
+			key = "Tab",
+			action = act.Multiple({
+				act.SendKey({ mods = "CTRL", key = "b" }),
+				act.SendKey({ key = "n" }),
+			}),
+		},
+
+		{
+			mods = "CTRL|SHIFT",
+			key = "Tab",
+			action = act.Multiple({
+				act.SendKey({ mods = "CTRL", key = "b" }),
+				act.SendKey({ key = "n" }),
+			}),
+		},
+
+		-- FIX: disable binding
+		-- {
+		-- 	mods = "CMD",
+		-- 	key = "`",
+		-- 	action = act.Multiple({
+		-- 		act.SendKey({ mods = "CTRL", key = "b" }),
+		-- 		act.SendKey({ key = "n" }),
+		-- 	}),
+		-- },
+
+		{
+			mods = "CMD",
+			key = "~",
+			action = act.Multiple({
+				act.SendKey({ mods = "CTRL", key = "b" }),
+				act.SendKey({ key = "p" }),
+			}),
+		},
 	},
 }
+
+wezterm.on("user-var-changed", function(window, pane, name, value)
+	-- local appearance = window:get_appearance()
+	-- local is_dark = appearance:find("Dark")
+	local overrides = window:get_config_overrides() or {}
+	wezterm.log_info("name", name)
+	wezterm.log_info("value", value)
+
+	if name == "T_SESSION" then
+		local session = value
+		wezterm.log_info("is session", session)
+		overrides.background = {
+			w.set_tmux_session_wallpaper(value),
+			{
+				source = {
+					Gradient = {
+						colors = { "#000000" },
+					},
+				},
+				width = "100%",
+				height = "100%",
+				opacity = 0.95,
+			},
+		}
+	end
+
+	if name == "ZEN_MODE" then
+		local incremental = value:find("+")
+		local number_value = tonumber(value)
+		if incremental ~= nil then
+			while number_value > 0 do
+				window:perform_action(wezterm.action.IncreaseFontSize, pane)
+				number_value = number_value - 1
+			end
+		elseif number_value < 0 then
+			window:perform_action(wezterm.action.ResetFontSize, pane)
+			overrides.font_size = nil
+		else
+			overrides.font_size = number_value
+		end
+	end
+	if name == "DIFF_VIEW" then
+		local incremental = value:find("+")
+		local number_value = tonumber(value)
+		if incremental ~= nil then
+			while number_value > 0 do
+				window:perform_action(wezterm.action.DecreaseFontSize, pane)
+				number_value = number_value - 1
+			end
+			-- overrides.background = {
+			-- 	w.set_nvim_wallpaper("Diffview.jpeg"),
+			--
+			-- 	{
+			-- 		source = {
+			-- 			Gradient = {
+			-- 				colors = { "#000000" },
+			-- 			},
+			-- 		},
+			-- 		width = "100%",
+			-- 		height = "100%",
+			-- 		opacity = 0.95,
+			-- 	},
+			-- }
+		elseif number_value < 0 then
+			window:perform_action(wezterm.action.ResetFontSize, pane)
+			overrides.background = nil
+			overrides.font_size = nil
+		else
+			overrides.font_size = number_value
+		end
+	end
+	window:set_config_overrides(overrides)
+end)
+
+return config
