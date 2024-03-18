ab48fbf59f9333818e0b22e7d7240f29062bb58f
lowranceworks <lowranceworks@gmail.com>
2024-03-18 14:18:11 -0500

    chore: update fish

--- files/Home/.config/fish/conf.d/abbr.fish
+++ files/Home/.config/fish/conf.d/abbr.fish
@@ -179,6 +179,13 @@ abbr tp "t --repo (pbpaste)"
 abbr tn "tmux new -s (basename (pwd))"
 abbr tt "touch .t && chmod +x .t && echo -e '#!/usr/bin/env bash\n' > .t && nvim .t"
 
+abbr tfa "terraform apply"
+abbr tfd "terraform destroy"
+abbr tfi "terraform init"
+abbr tfm "terraform format"
+abbr tfs "terraform state"
+abbr tfsl "terraform state list"
+
 abbr v "nvim +GoToFile"
 abbr vfzf "nvim (fd --type f --hidden --follow --exclude .git | fzf-tmux -p -w 100 --reverse --preview 'bat --color=always --style=numbers --line-range=:500 {}')"
 abbr va "nvim ~/.config/alacritty/alacritty.yml"
