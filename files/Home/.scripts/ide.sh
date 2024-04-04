985a3a7895d80645c0d4e6a4581340604d3f88b3
lowranceworks <lowranceworks@gmail.com>
2024-04-04 16:53:18 -0500

    feat: add "ide" function that splits tmux panes

--- files/Home/.scripts/ide.sh
+++ files/Home/.scripts/ide.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 tmux split-window -h -l 66%
+tmux select-pane -t 0
+tmux split-window -v
+tmux send-keys -t 1 'lazygit' Enter
+tmux send-keys -t 0 'clear' Enter
+tmux send-keys -t 2 'nvim .' Enter
